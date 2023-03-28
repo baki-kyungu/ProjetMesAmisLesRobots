9fe25e28b9574fcda203a6d50458fdb4f376888c
fabuleux366 <abbakikyungu@gmail.com>
2023-03-28 17:41:07 -0400

    fini interface user

--- mesamislesrobots/src/App.js
+++ mesamislesrobots/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [robots, setRobots] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
+      setRobots(data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="rb--container">
+      <div className="header--container">
+        <span className="cardTitle">MES AMIS ROBOTS</span>
+        <input className="search" placeholder="recherche par nom" />
+      </div>
+
+      {robots.map((robot) => (
+        <div class="card--container">
+          <img
+            src={`https://robohash.org/${robot.id}`}
+            className="rb-image"
+            alt=""
+            srcset=""
+          />
+          <h4 className="subTitle">{robot.name}</h4>
+          <p className="rbSubTitle">{robot.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
